6758509c3e0bc4d99ab4c700c3c82f2182b123fb
MaSidrenko <nexon863@gmail.com>
2025-12-30 03:46:30 +0300

    Orange Site

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,28 +29,52 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          light: "#FAF8F8", // фоновый светлый
+          lightgray: "#E5E5E5", // светло-серый для блоков
+          gray: "#B8B8B8", // текст серый
+          darkgray: "#4E4E4E", // текст заголовков
+          dark: "#2B2B2B", // основной текст
+          secondary: "#D97B36", // оранжевый акцент
+          tertiary: "#C7C7C7", // дополнительные элементы
+          highlight: "rgba(217, 123, 54, 0.15)", // подсветка оранжевого
+          textHighlight: "#D97B36AA", // подсветка текста
         },
         darkMode: {
-          light: "#161618",
+          light: "#161618", // фон темный
           lightgray: "#393639",
           gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#b3aa0288",
+          darkgray: "#D4D4D4",
+          dark: "#EBEBEC",
+          secondary: "#D97B36", // оранжевый акцент
+          tertiary: "#4E4E4E",
+          highlight: "rgba(217, 123, 54, 0.15)",
+          textHighlight: "#D97B36AA",
         },
       },
+      // colors: {
+      //   lightMode: {
+      //     light: "#faf8f8",
+      //     lightgray: "#e5e5e5",
+      //     gray: "#b8b8b8",
+      //     darkgray: "#4e4e4e",
+      //     dark: "#2b2b2b",
+      //     secondary: "#284b63",
+      //     tertiary: "#84a59d",
+      //     highlight: "rgba(143, 159, 169, 0.15)",
+      //     textHighlight: "#fff23688",
+      //   },
+      //   darkMode: {
+      //     light: "#161618",
+      //     lightgray: "#393639",
+      //     gray: "#646464",
+      //     darkgray: "#d4d4d4",
+      //     dark: "#ebebec",
+      //     secondary: "#7b97aa",
+      //     tertiary: "#84a59d",
+      //     highlight: "rgba(143, 159, 169, 0.15)",
+      //     textHighlight: "#b3aa0288",
+      //   },
+      // },
     },
   },
   plugins: {
